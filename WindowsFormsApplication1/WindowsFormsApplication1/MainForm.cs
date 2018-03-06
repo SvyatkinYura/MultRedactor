a64d7cc6a0265cf6d88ff44da2b36e3298fa467d
Zifrkoks <31888982+Zifrkoks@users.noreply.github.com>
2018-03-06 17:32:54 +0400

    Add files via upload

--- WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -256,5 +256,20 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
